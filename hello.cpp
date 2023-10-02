a6977125836b3c2ebb435900e291b2e209503e93
Kanhaiya Kumar Rai <114878466+kanhaiyarajput20@users.noreply.github.com>
2023-10-02 11:10:14 +0530

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,9 +1,7 @@
 #include <iostream>
-using namespace std;
 
-int main() 
-{
+int main() {
     // Print "Hello, World!" to the console
-    cout << "Hello, World!" << endl;
+    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
